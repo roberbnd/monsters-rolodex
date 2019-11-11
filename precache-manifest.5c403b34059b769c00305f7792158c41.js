cbb0007c878636ef47f73b023bd4a32654578fc0
bnd <banda1915@gmail.com>
2019-11-11 16:42:53 -0600

    Updates

---
+++ precache-manifest.5c403b34059b769c00305f7792158c41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef8e14dd446b3c440d0fce25fdc2c7cc",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "2dea253c3709129013e2",
+    "url": "/monster-rolodex/static/css/main.08bde931.chunk.css"
+  },
+  {
+    "revision": "8f66e7892f88ac220728",
+    "url": "/monster-rolodex/static/js/2.e8d38444.chunk.js"
+  },
+  {
+    "revision": "2dea253c3709129013e2",
+    "url": "/monster-rolodex/static/js/main.c98fe08d.chunk.js"
+  },
+  {
+    "revision": "18a51a7a4553ed52107d",
+    "url": "/monster-rolodex/static/js/runtime-main.9db40554.js"
+  }
+]);
